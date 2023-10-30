3c0bdb9cd406513a697d071f9b7d6482cfe0bb41
Sachita Nand <shachitanandk@gmail.com>
2023-10-30 15:07:14 +0530

    .

--- key001.sh
+++ key001.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Formatting variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+
+
+
 # Directory to hold the files
 DIR_NAME="secured_scripts"
 
@@ -15,11 +25,27 @@ echo "${VAR_NAME}"
 # Fetch the key from the derived environment variable
 DECRYPTION_KEY="${!VAR_NAME}"
 
+# Function to display a message in green
+info_msg() {
+    echo -e "${GREEN}${BOLD}$1${NC}"
+}
+
+# Function to display a warning in yellow
+warning_msg() {
+    echo -e "${YELLOW}${BOLD}$1${NC}"
+}
+
+# Function to display an error in red
+error_msg() {
+    echo -e "${RED}${BOLD}$1${NC}"
+}
+
+
 
 
 # Check if the key variable is set
 if [ -z "$DECRYPTION_KEY" ]; then
-    echo "Error: The decryption key is not set."
+    error_msg "Error: The decryption key is not set."
     exit 1
 fi
 
@@ -33,19 +59,19 @@ cd "$DIR_NAME"
 
 # Function to download the encrypted script
 download_script() {
-    echo "Downloading the encrypted script..."
+    info_msg "Downloading the encrypted script..."
     wget -O script.js.enc $SCRIPT_URL
 }
 
 # Function to download the version file
 download_version() {
-    echo "Downloading the version file..."
+    info_msg "Downloading the version file..."
     wget -O version.json $VERSION_URL
 }
 
 # Check if version.json exists
 if [ ! -f version.json ]; then
-    echo "Version file not found!"
+    warning_msg "Version file not found!"
     download_version
     download_script
 else
@@ -55,16 +81,16 @@ else
     NEW_VERSION=$(cat version.json | grep version | awk -F '"' '{print $4}')
 
     if [ "$CURRENT_VERSION" != "$NEW_VERSION" ]; then
-        echo "Version has changed from $CURRENT_VERSION to $NEW_VERSION. Updating script..."
+        info_msg "Version has changed from $CURRENT_VERSION to $NEW_VERSION. Updating script..."
         download_script
     else
-        echo "You already have the latest version of the script."
+        info_msg "You already have the latest version of the script."
     fi
 fi
 
 # Check if script.js.enc exists, if not, download
 if [ ! -f script.js.enc ]; then
-    echo "Encrypted script not found!"
+    warning_msg "Encrypted script not found!"
     download_script
 fi
 
@@ -72,4 +98,5 @@ fi
 openssl enc -aes-256-cbc -d -in script.js.enc -out script.js -k $DECRYPTION_KEY
 
 # Execute the Node.js script
+info_msg "Executing the script..."
 node script.js
